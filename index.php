8c2e4de1f89c629e087c4185bb8baa43ea660f74
Kobe Christiaensen <kobe.christiaensen@hotmail.com>
2018-05-02 11:08:51 +0200

    index html done

--- index.php
+++ index.php
@@ -5,10 +5,75 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gubl</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     
+    <nav>
+
+        <ul>
+        <li><img src="#" alt="logo" class="nav nav_logo"></li>
+        <li><a href="login.php" class="nav nav_logo">Login</a></li>
+        </ul>
+
+    </nav>
+    
+
+    <section class="page page_home">
+
+        <div class="page_home_quote">
+        
+        <H1>WIJ HELPEN
+        <br>U een handje verder</H1>
+        
+        </div>
+
+        <div class="page_home_CTA"></div>
+
+    </section>
+
+    <section class="page page_gubl">
+        <div class="div_white">
+        <img src="#" alt="Gubl, Group up Buy Low!">
+        </div>
+
+        <div class="text text_gubl">
+        <p>Gubl is een groepsverkoop website die zich specialiseert in de verkoop van noodzakelijke artikelen. Dit is gericht op de Mechelse bevolking met een leefloon. De verkochte artikels komen van lokale handelaars.</p>
+        </div>    
+    </section>
+
+
+    <section class="page page_what">
+
+        <div class="text text_what">
+        <h2 class="text_blue">Wat doen we?</h2>
+        <p>Gubl bestaat uit twee kanten, een platform voor de koper en een dashboard voor de verkoper. De verkopers krijgen de vrijheid zelf te posten wat en wanneer ze willen. De kopers kiezen wat hun interesseert.</p>
+        </div>
+
+
+    </section>
+
 
     
+    <section class="page page_who">
+
+        <div class="text text_who">
+            <h2 class="text_white">wie zijn we?</h2>
+
+            <div class="person person_1">
+                <img class="person_img" src="#" alt="Kobe Christiaensen">
+                <p class="text_white">Kobe Christiaensen</p>
+            </div>
+
+            <div class="person person_2">
+                <img class="person_img" src="#" alt="Jimmy Truyts">
+                <p class="text_white">Jimmy Truyts</p>
+            </div>
+
+            <p class="text_white">Studenten Thomas More Mechelen wij kregen de opdracht een smart-city project uit te werken.</p>
+        </div>
+
+    </section>
 </body>
 </html>
